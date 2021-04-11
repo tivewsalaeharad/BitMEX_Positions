5f6dd1b0e8e888f509c387cd8fb26f90af3cbcdb
tivew <tivew@mail.ru>
2021-04-11 22:32:34 +0300

    Подключение к WebSocket

--- BitMEX_Positions/PositionsForm.cs
+++ BitMEX_Positions/PositionsForm.cs
@@ -1,5 +1,10 @@
-﻿using System;
+﻿using Bitmex.Client.Websocket;
+using Bitmex.Client.Websocket.Client;
+using Bitmex.Client.Websocket.Requests;
+using Bitmex.Client.Websocket.Websockets;
+using System;
 using System.Reflection;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace BitMEX_Positions
@@ -12,7 +17,55 @@ namespace BitMEX_Positions
         {
             InitializeComponent();
             Load += new EventHandler(Form1_Load);
+
+            //Connecting to WebSocket
+            var url = BitmexValues.ApiWebsocketUrl;
+            using (var communicator = new BitmexWebsocketCommunicator(url))
+            {
+                communicator.Name = "Bitmex-1";
+                communicator.ReconnectTimeout = TimeSpan.FromMinutes(10);
+                communicator.ReconnectionHappened.Subscribe(type =>
+                    Console.WriteLine($"Reconnection happened, type: {type.Type}"));
+
+                using (var client = new BitmexWebsocketClient(communicator))
+                {
+
+                    client.Streams.InfoStream.Subscribe(info =>
+                    {
+                        Console.WriteLine($"Reconnection happened, Message: {info.Info}, Version: {info.Version:D}");
+                        SendSubscriptionRequests(client).Wait();
+                    });
+
+                    SubscribeToStreams(client);
+
+                    communicator.Start();
+
+                    _ = StartPinging(client);
+                    
+                }
+            }
+
         }
+
+        private static async Task StartPinging(BitmexWebsocketClient client)
+        {
+            await Task.Delay(TimeSpan.FromSeconds(30));
+            client.Send(new PingRequest());
+        }
+
+        private static async Task SendSubscriptionRequests(BitmexWebsocketClient client)
+        {
+            client.Send(new PingRequest());
+            if (!string.IsNullOrWhiteSpace(AccessKeys.bitmexSecret))
+                client.Send(new AuthenticationRequest(AccessKeys.bitmexKey, AccessKeys.bitmexSecret));
+        }
+
+
+        private void SubscribeToStreams(BitmexWebsocketClient client)
+        {
+            client.Streams.PositionStream.Subscribe(y => PopulateDataGridView());
+        }
+
 
         private void Form1_Load(object sender, EventArgs e)
         {
@@ -41,14 +94,18 @@ namespace BitMEX_Positions
 
             foreach (PositionItem position in positions)
             {
-                object[] data = new object[PositionProperties.Length];
-
-                for (int i = 0; i < PositionProperties.Length; i++)
+                if (position.isOpen)
                 {
-                    data[i] = PositionProperties[i].GetValue(position);
-                }
+                    object[] data = new object[PositionProperties.Length];
 
-                PositionsGrid.Rows.Add(data);
+                    for (int i = 0; i < PositionProperties.Length; i++)
+                    {
+                        data[i] = PositionProperties[i].GetValue(position);
+                    }
+
+                    PositionsGrid.Rows.Add(data);
+
+                }
 
             }
 
